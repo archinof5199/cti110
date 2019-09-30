78177e0dcb26b2b947be968859032ead22167a12
archinof5199 <55547941+archinof5199@users.noreply.github.com>
2019-09-29 23:06:44 -0400

    Add files via upload

---
+++ P3LAB_Debugging_FrancescfoArchino.py
@@ -0,0 +1,22 @@
+# CTI-110
+# P3LAB - Debugging
+# Francesco Archino
+# 9/29/2019
+#
+#This program converts numeric grades into letter grades
+
+if score >= A_score:
+    print("Your grade is A.")
+else:
+    if score >= B_score:
+    print("Your grade is a B.")
+    else:
+        if score >= C_score:
+        print("Your grade is a C.")
+        else:
+            if score >= D_score:
+            print("Your grade is a D.")
+            else:
+                print("Your grade is a F.")
+  
+         
